c2dca560a4090ee78544464a091f91b5ef6e554b
Zheng Hanming <hanming2033@gmail.com>
2018-05-03 23:17:56 +0800

    added antd form

--- src/components/Ordering/ContactForm.tsx
+++ src/components/Ordering/ContactForm.tsx
@@ -3,20 +3,23 @@ import styled from 'styled-components'
 import { IBurgerProps } from '../BurgerBuilder/BurgerDisplay/Burger'
 import orderAxios from '../../http/axios-order'
 import { RouteComponentProps } from 'react-router-dom'
-import { Progress, Button } from 'antd'
-import handleHttpError from '../../http/handleHttpError'
-// import Input from '../.elements/Input'
+import { Form, Select, Icon, Input, Progress, Button } from 'antd'
+// import handleHttpError from '../../http/handleHttpError'
+import { FormComponentProps } from 'antd/lib/form'
+const FormItem = Form.Item
 
 export interface IContactInfoProps {
   totalPrice: number
 }
 
 export interface IContactInfoState {
-  name: string
-  email: string
-  address: {
+  orderForm: {
+    name: string
     street: string
-    postalCode: string
+    zipCode: string
+    country: string
+    email: string
+    deliveryMethod: string
   }
   loading: boolean
 }
@@ -30,8 +33,25 @@ const Wrapper = styled.div`
   }
 `
 
-class ContactInfo extends React.Component<IContactInfoProps & IBurgerProps & RouteComponentProps<{}>, IContactInfoState> {
-  public state = { name: '', email: '', address: { street: '', postalCode: '' }, loading: false }
+function hasErrors(fieldsError: any) {
+  return Object.keys(fieldsError).some(field => fieldsError[field])
+}
+
+class ContactInfo extends React.Component<
+  IContactInfoProps & IBurgerProps & RouteComponentProps<{}> & FormComponentProps,
+  IContactInfoState
+> {
+  public state = {
+    orderForm: {
+      name: '',
+      street: '',
+      zipCode: '',
+      country: '',
+      email: '',
+      deliveryMethod: ''
+    },
+    loading: false
+  }
 
   public handlerOrder = (e: React.FormEvent<any>) => {
     this.setState({ loading: true })
@@ -56,25 +76,92 @@ class ContactInfo extends React.Component<IContactInfoProps & IBurgerProps & Rou
       })
   }
 
+  public componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields()
+  }
+
+  public handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+        this.setState({ orderForm: values })
+        // TODO to continue from here.
+        // TODO document some tips
+        // TODO combine other orders and ajax
+      }
+    })
+  }
+
   public render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form
+    // Only show error after a field is touched.
+    const nameError = isFieldTouched('name') && getFieldError('name')
+    const streetError = isFieldTouched('street') && getFieldError('street')
+    const zipCodeError = isFieldTouched('zipCode') && getFieldError('zipCode')
+    const countryError = isFieldTouched('country') && getFieldError('country')
+    const emailError = isFieldTouched('email') && getFieldError('email')
+    const deliveryError = isFieldTouched('delivery') && getFieldError('delivery')
+
+    console.log(this.props.form.getFieldsValue())
+
     return (
       <Wrapper>
         {this.state.loading && <Progress percent={50} status="active" />}
-
-        <h4>Enter you contact information</h4>
-        <form>
-          <input type="text" name="name" placeholder="Your Name" />
-          <input type="email" name="email" placeholder="Your Email" />
-          {/* <Input inputType="input" label="Email" type="email" name="email" placeholder="Your Email" /> */}
-          <input type="text" name="street" placeholder="Street" />
-          <input type="text" name="postal" placeholder="Postal Code" />
-          <Button type="danger" onClick={e => this.handlerOrder(e)}>
-            Submit
-          </Button>
-        </form>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem validateStatus={emailError ? 'error' : ''} help={emailError || ''}>
+            {getFieldDecorator('email', {
+              rules: [{ pattern: /.+@.+/, required: true, message: 'Please input your email!' }]
+            })(<Input type="email" prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Emaill Address" />)}
+          </FormItem>
+          <FormItem validateStatus={nameError ? 'error' : ''} help={nameError || ''}>
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: 'Please input your name!' }]
+            })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Name" />)}
+          </FormItem>
+          <FormItem validateStatus={streetError ? 'error' : ''} help={streetError || ''}>
+            {getFieldDecorator('street', {
+              rules: [{ required: true, message: 'Please input your street!' }]
+            })(<Input prefix={<Icon type="environment-o" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Street" />)}
+          </FormItem>
+          <FormItem validateStatus={zipCodeError ? 'error' : ''} help={zipCodeError || ''}>
+            {getFieldDecorator('zipCode', {
+              rules: [{ required: true, message: 'Please input your zip code!' }]
+            })(<Input prefix={<Icon type="bars" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Zip Code" />)}
+          </FormItem>
+          <FormItem validateStatus={countryError ? 'error' : ''} help={countryError || ''}>
+            {getFieldDecorator('country', {
+              rules: [{ required: true, message: 'Please select your country!' }]
+            })(
+              <Select placeholder="Please select a country">
+                <Select.Option value="china">China</Select.Option>
+                <Select.Option value="usa">U.S.A</Select.Option>
+                <Select.Option value="singapore">Singapore</Select.Option>
+              </Select>
+            )}
+          </FormItem>
+          <FormItem validateStatus={deliveryError ? 'error' : ''} help={deliveryError || ''}>
+            {getFieldDecorator('delivery', {
+              rules: [{ required: true, message: 'Please select your delivery method!' }]
+            })(
+              <Select placeholder="Please select a delivery method">
+                <Select.Option value="singpost">Singpost</Select.Option>
+                <Select.Option value="fedex">Fedex</Select.Option>
+              </Select>
+            )}
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+              Order
+            </Button>
+          </FormItem>
+        </Form>
       </Wrapper>
     )
   }
 }
 
-export default handleHttpError(ContactInfo, orderAxios)
+export default Form.create()(ContactInfo)
+
+// export default handleHttpError(ContactInfo, orderAxios)
