3f159a9df50a7ec701a80f0c83f4e227a7eef1e4
Zheng Hanming <hanming2033@gmail.com>
2018-05-03 08:26:18 +0800

    another test comment

--- src/components/.elements/Input.tsx
+++ src/components/.elements/Input.tsx
@@ -36,6 +36,7 @@ const Input: React.SFC<IInputProps> = (props: any) => {
   // set up dynamic input element based on the props passed in
   // this is good to minimize the code in the main component and render the form dynamically with state
   // test2 comment
+  // one more comment
   let inputElement = null
   switch (props.inputType) {
     case 'input':
